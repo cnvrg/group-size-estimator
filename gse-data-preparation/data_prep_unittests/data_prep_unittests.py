3015f382855b4e220c8746af5643b0c89557496b
sjagtap1803 <siddhant.jagtap@intel.com>
2022-11-16 17:55:10 -0600

    added pragma tags + black formatting

--- gse-data-preparation/data_prep_unittests/data_prep_unittests.py
+++ gse-data-preparation/data_prep_unittests/data_prep_unittests.py
@@ -6,7 +6,15 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import numpy as np
 import unittest
 import yaml
-from data_preparation import NoneDatasetError, DatasetPathError, ClassFileFormatError, ValidationSizeError, DatasetSizeError, DatasetNamingError, NumberOfClassesError
+from data_preparation import (
+    NoneDatasetError,
+    DatasetPathError,
+    ClassFileFormatError,
+    ValidationSizeError,
+    DatasetSizeError,
+    DatasetNamingError,
+    NumberOfClassesError,
+)
 from data_preparation import validate_arguments, validate_dataset, train_valid_split
 
 np.random.seed(2)
@@ -18,18 +26,29 @@ class TestDataPreparation(unittest.TestCase):
         # Read config file for unittesting
         with open("./test_config.yaml", "r") as file:
             config = yaml.load(file, Loader=yaml.FullLoader)
-        
+
         # Define paths from config file for validate_arguments function
         self.dataset = config["dataset_dir"]
-        self.data_dir= config["dataset_full"]
+        self.data_dir = config["dataset_full"]
         self.img_dir = config["dataset_images"]
         self.lbl_dir = config["dataset_labels"]
         self.class_txt = config["classes_txt"]
         self.class_csv = config["classes_csv"]
-        
+
         # Define input arguments and expected answers for validate_dataset function
         self.img_formats = ["jpg"]
-        self.data_list = ["1.jpg", "1.txt", "2.jpg", "2.txt", "3.jpg", "3.txt", "4.jpg", "4.txt", "5.jpg", "5.txt"]
+        self.data_list = [
+            "1.jpg",
+            "1.txt",
+            "2.jpg",
+            "2.txt",
+            "3.jpg",
+            "3.txt",
+            "4.jpg",
+            "4.txt",
+            "5.jpg",
+            "5.txt",
+        ]
         self.img_list = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"]
         self.lbl_list = ["1.txt", "2.txt", "3.txt", "4.txt", "5.txt"]
         self.class_list = ["oranges", "apples", "bananas"]
@@ -56,14 +75,14 @@ class TestValidateArguments(TestDataPreparation):
         """Checks for NoneDatasetError if all directories are None"""
         with self.assertRaises(NoneDatasetError):
             validate_arguments("None", "None", "None", "classes.txt", "0.1")
-    
+
     def test_dataset_path_error(self):
         """Checks for DatasetPathError if the images directory is None and labels directory is not or vice-versa"""
         with self.assertRaises(DatasetPathError):
             validate_arguments("None", self.img_dir, "None", "classes.txt", "0.1")
         with self.assertRaises(DatasetPathError):
             validate_arguments("None", "None", self.lbl_dir, "classes.txt", "0.1")
-    
+
     def test_class_file_format_error(self):
         """Checks for ClassFileFormatError if classes file is not in txt or csv format"""
         with self.assertRaises(ClassFileFormatError):
@@ -72,55 +91,85 @@ class TestValidateArguments(TestDataPreparation):
     def test_validation_size_error(self):
         """Checks for ValidationSizeError if validation size is not between 0 and 0.4"""
         with self.assertRaises(ValidationSizeError):
-            validate_arguments("None", self.img_dir, self.lbl_dir, self.class_txt, "0.6")
+            validate_arguments(
+                "None", self.img_dir, self.lbl_dir, self.class_txt, "0.6"
+            )
 
 
 class TestValidateDataset(TestDataPreparation):
     def test_data_dir_input(self):
         """Checks for appropriate output when both images and labels are present in one directory"""
-        result = validate_dataset(self.data_list, [], [], self.img_formats, self.class_list)
+        result = validate_dataset(
+            self.data_list, [], [], self.img_formats, self.class_list
+        )
         self.assertIsInstance(result[0], list)
         self.assertIsInstance(result[1], list)
-        self.assertEqual([len(result[0]), len(result[1])], [self.num_images, self.num_labels])
-    
+        self.assertEqual(
+            [len(result[0]), len(result[1])], [self.num_images, self.num_labels]
+        )
+
     def test_img_lbl_input(self):
         """Checks for appropriate output when images and labels are present in different directories"""
-        result = validate_dataset([], self.img_list, self.lbl_list, self.img_formats, self.class_list)
+        result = validate_dataset(
+            [], self.img_list, self.lbl_list, self.img_formats, self.class_list
+        )
         self.assertIsInstance(result[0], list)
         self.assertIsInstance(result[1], list)
-        self.assertEqual([len(result[0]), len(result[1])], [self.num_images, self.num_labels])
+        self.assertEqual(
+            [len(result[0]), len(result[1])], [self.num_images, self.num_labels]
+        )
 
     def test_dataset_size_error(self):
         """Checks for DatasetSizeError if number of images is not equal to number of labels"""
         with self.assertRaises(DatasetSizeError):
-            validate_dataset([], self.img_list_v1, self.lbl_list_v1, self.img_formats, self.class_list)
+            validate_dataset(
+                [],
+                self.img_list_v1,
+                self.lbl_list_v1,
+                self.img_formats,
+                self.class_list,
+            )
 
     def test_dataset_naming_error(self):
         """Checks for DatasetNamingError if an image does not have a corresponding label"""
         with self.assertRaises(DatasetNamingError):
-            validate_dataset([], self.img_list_v1, self.lbl_list_v2, self.img_formats, self.class_list)
-    
+            validate_dataset(
+                [],
+                self.img_list_v1,
+                self.lbl_list_v2,
+                self.img_formats,
+                self.class_list,
+            )
+
     def test_number_of_classes_error(self):
         """Checks for NumberOfClassesError if no classes/categories have been defined in classes file"""
         with self.assertRaises(NumberOfClassesError):
-            validate_dataset([], self.img_list, self.lbl_list, self.img_formats, self.class_list_v1)
+            validate_dataset(
+                [], self.img_list, self.lbl_list, self.img_formats, self.class_list_v1
+            )
 
 
 class TrainValidSplit(TestDataPreparation):
     def test_train_valid_sets(self):
         """Checks datatype and sizes of train and validation sets"""
-        train_imgs, val_imgs, train_lbls, val_lbls = train_valid_split(self.img_list, self.lbl_list, self.valid_size)
+        train_imgs, val_imgs, train_lbls, val_lbls = train_valid_split(
+            self.img_list, self.lbl_list, self.valid_size
+        )
         self.assertIsInstance(train_imgs, list)
         self.assertIsInstance(val_imgs, list)
         self.assertIsInstance(train_lbls, list)
         self.assertIsInstance(val_lbls, list)
-        self.assertEqual([len(train_imgs), len(val_imgs), len(train_lbls), len(val_lbls)], [self.num_train, self.num_valid, self.num_train, self.num_valid])
+        self.assertEqual(
+            [len(train_imgs), len(val_imgs), len(train_lbls), len(val_lbls)],
+            [self.num_train, self.num_valid, self.num_train, self.num_valid],
+        )
 
     def test_exact_output(self):
         """Checks if the training and validation sets are generated as expected"""
-        train_imgs, val_imgs, train_lbls, val_lbls = train_valid_split(self.img_list, self.lbl_list, self.valid_size)
+        train_imgs, val_imgs, train_lbls, val_lbls = train_valid_split(
+            self.img_list, self.lbl_list, self.valid_size
+        )
         self.assertEqual(train_imgs, self.train_imgs)
         self.assertEqual(val_imgs, self.valid_imgs)
         self.assertEqual(train_lbls, self.train_lbls)
         self.assertEqual(val_lbls, self.valid_lbls)
-        
--- gse-data-preparation/data_preparation.py
+++ gse-data-preparation/data_preparation.py
@@ -13,27 +13,33 @@ class NoneDatasetError(Exception):
 
     def __init__(self):
         super().__init__()
-    
+
     def __str__(self):
         return "NoneDatasetError: No dataset provided. The first three input arguments cannot be None!"
 
 
 class DatasetPathError(Exception):
     """Raise if either the images directory or labels directory is None"""
+
     def __init__(self):
         super().__init__()
-    
+
     def __str__(self):
-        return "DatasetPathError: The images directory or label directory cannot be None!"
+        return (
+            "DatasetPathError: The images directory or label directory cannot be None!"
+        )
 
 
 class ClassFileFormatError(Exception):
     """Raise if the classes file is not in txt or csv format"""
+
     def __init__(self):
         super().__init__()
-    
+
     def __str__(self):
-        return "ClassFileFormatError: The classes file needs to be in txt or csv format!"
+        return (
+            "ClassFileFormatError: The classes file needs to be in txt or csv format!"
+        )
 
 
 class ValidationSizeError(Exception):
@@ -49,17 +55,19 @@ class ValidationSizeError(Exception):
 
 class DatasetSizeError(Exception):
     """Raise if number of images is not equal to number of labels/annotations"""
+
     def __init__(self, num_images, num_labels):
         super().__init__(num_images, num_labels)
         self.num_images = num_images
         self.num_labels = num_labels
-    
+
     def __str__(self):
         return f"DatasetSizeError: The number of images should be equal to number of labels/annotations. Your dataset contains {self.num_images} images and {self.num_labels} labels"
 
 
 class NumberOfClassesError(Exception):
     """Raise if number of classes is less than 2"""
+
     def __init__(self, num_classes):
         super().__init__(num_classes)
         self.num_classes = num_classes
@@ -70,62 +78,111 @@ class NumberOfClassesError(Exception):
 
 class DatasetNamingError(Exception):
     """Raise if an image does not have a corresponding label"""
+
     def __init__(self, image):
         super().__init__(image)
         self.image = image
-    
+
     def __str__(self):
         return f"DatasetNamingError: No label found for image {self.image}. Images and labels should have the same file names without extensions!"
 
 
-def parse_parameters():
+def parse_parameters():  # pragma: no cover
     """Command line parser."""
     parser = argparse.ArgumentParser(description="""Dataset Preparation""")
-    parser.add_argument("--data_dir", action="store", dest="data_dir", required=True, help="""--- Path to directory containing both images and labels ---""")
-    parser.add_argument("--image_dir", action="store", dest="image_dir", required=True, help="""--- Path to directory containing images ---""")
-    parser.add_argument("--label_dir", action="store", dest="label_dir", required=True, help="""--- Path to directory containing labels/annotations ---""")
-    parser.add_argument("--class_file", action="store", dest="class_file", required=True, help="""--- Path to .txt or .csv file containing class names ---""")
-    parser.add_argument("--output_dir", action="store", dest="output_dir", required=False, default=cnvrg_workdir, help="""--- The path to save library artifacts to ---""")
-    parser.add_argument("--valid_size", action="store", dest="valid_size", required=False, default=0.1, help="""--- Size of validation set as percentage of entire dataset ---""")
+    parser.add_argument(
+        "--data_dir",
+        action="store",
+        dest="data_dir",
+        required=True,
+        help="""--- Path to directory containing both images and labels ---""",
+    )
+    parser.add_argument(
+        "--image_dir",
+        action="store",
+        dest="image_dir",
+        required=True,
+        help="""--- Path to directory containing images ---""",
+    )
+    parser.add_argument(
+        "--label_dir",
+        action="store",
+        dest="label_dir",
+        required=True,
+        help="""--- Path to directory containing labels/annotations ---""",
+    )
+    parser.add_argument(
+        "--class_file",
+        action="store",
+        dest="class_file",
+        required=True,
+        help="""--- Path to .txt or .csv file containing class names ---""",
+    )
+    parser.add_argument(
+        "--output_dir",
+        action="store",
+        dest="output_dir",
+        required=False,
+        default=cnvrg_workdir,
+        help="""--- The path to save library artifacts to ---""",
+    )
+    parser.add_argument(
+        "--valid_size",
+        action="store",
+        dest="valid_size",
+        required=False,
+        default=0.1,
+        help="""--- Size of validation set as percentage of entire dataset ---""",
+    )
     return parser.parse_args()
 
 
-def validate_arguments(data_directory, img_directory, lbl_directory, cls_file, valid_size):
+def validate_arguments(
+    data_directory, img_directory, lbl_directory, cls_file, valid_size
+):
     """Validates input arguments
-    
+
     Checks if the input arguments provided by the user are appropriate for running this library.
     Makes sure that the first three arguments are not None and that the class file is in .txt or .csv format
-    
+
     Args:
         data_directory: The directory containing both images as well as labels
         img_directory: The directory containing just images
         lbl_directory: The directory containing just labels/annotations
         cls_file: The file containing pre-defined classes/categories for object detection
         valid_size: Size of validation set as percentage of entire dataset
-    
+
     Raises:
         NoneDatasetError: If first three input arguments are all None
         DatasetPathError: If either images directory or labels directory is None
         ClassFileFormatError: If the classes file is not in txt or csv format
         ValidationSizeError: If validation size is not between 0.0 and 0.4
     """
-    if data_directory.lower() == "none" and img_directory.lower() == "none" and lbl_directory.lower() == "none":
+    if (
+        data_directory.lower() == "none"
+        and img_directory.lower() == "none"
+        and lbl_directory.lower() == "none"
+    ):
         raise NoneDatasetError
 
     if data_directory.lower() == "none":
-        if (img_directory.lower() != "none" and lbl_directory.lower() == "none") or (img_directory.lower() == "none" and lbl_directory.lower() != "none"):
+        if (img_directory.lower() != "none" and lbl_directory.lower() == "none") or (
+            img_directory.lower() == "none" and lbl_directory.lower() != "none"
+        ):
             raise DatasetPathError
-    
+
     if not any(ext in cls_file for ext in [".txt", ".csv"]):
         raise ClassFileFormatError
 
     if not (float(valid_size) > 0 and float(valid_size) <= 0.4):
-        raise ValidationSizeError(float(valid_size))    
+        raise ValidationSizeError(float(valid_size))
 
 
-def validate_dataset(data_dir_list, img_dir_list, lbl_dir_list, img_formats, class_list):
+def validate_dataset(
+    data_dir_list, img_dir_list, lbl_dir_list, img_formats, class_list
+):
     """Validates the input dataset
-    
+
     Checks if number of images is same as number of labels.
     Raises an error if an image does not have a corresponding label/annotation file.
 
@@ -135,12 +192,12 @@ def validate_dataset(data_dir_list, img_dir_list, lbl_dir_list, img_formats, cla
         lbl_dir_list: list containing names of all files present in the label directory
         img_formats: A list containing image file formats compatible with YOLOv5
         class_list: A list containing all classes/categories in the dataset
-    
+
     Raises:
         DatasetSizeError: If number of images is not equal to number of label/annotation files
         DatasetNamingError: If an image does not have a label/annotation file associated with it
         NumberOfClassesError: If number of classes is less than 1
-    
+
     Returns:
         image_list: a list containing filenames for images
         label_list: a list containing filenames for labels/annotations
@@ -148,10 +205,14 @@ def validate_dataset(data_dir_list, img_dir_list, lbl_dir_list, img_formats, cla
     image_list, label_list = [], []
 
     if len(data_dir_list):
-        image_list = [file for file in data_dir_list if any(fmt in file for fmt in img_formats)]
+        image_list = [
+            file for file in data_dir_list if any(fmt in file for fmt in img_formats)
+        ]
         label_list = [file for file in data_dir_list if ".txt" in file]
     else:
-        image_list = [file for file in img_dir_list if any(fmt in file for fmt in img_formats)]
+        image_list = [
+            file for file in img_dir_list if any(fmt in file for fmt in img_formats)
+        ]
         label_list = [file for file in lbl_dir_list if ".txt" in file]
 
     if len(image_list) != len(label_list):
@@ -162,16 +223,16 @@ def validate_dataset(data_dir_list, img_dir_list, lbl_dir_list, img_formats, cla
         label_file = image_name + ".txt"
         if label_file not in label_list:
             raise DatasetNamingError(image)
-        
+
     if len(class_list) < 1:
         raise NumberOfClassesError(len(class_list))
-    
+
     return image_list, label_list
 
 
 def train_valid_split(images, labels, valid_size):
     """Splits data into training and validation sets
-    
+
     Args:
         images: a list containing filenames for images
         labels: a list containing filenames for labels/annotations
@@ -185,16 +246,28 @@ def train_valid_split(images, labels, valid_size):
     """
     images.sort()
     labels.sort()
-    train_images, val_images, train_labels, val_labels = train_test_split(images, labels, test_size=float(valid_size), random_state=42)
+    train_images, val_images, train_labels, val_labels = train_test_split(
+        images, labels, test_size=float(valid_size), random_state=42
+    )
     return train_images, val_images, train_labels, val_labels
 
 
-def prepare_dataset(data_directory, img_directory, lbl_directory, output_dir, train_images, val_images, train_labels, val_labels, config_dict):
+def prepare_dataset(
+    data_directory,
+    img_directory,
+    lbl_directory,
+    output_dir,
+    train_images,
+    val_images,
+    train_labels,
+    val_labels,
+    config_dict,
+):  # pragma: no cover
     """Creates training and validation directories and moves these to the output directory"""
-    train_img_dst = os.path.join(output_dir, config_dict['training_images_dir'])
-    train_lbl_dst = os.path.join(output_dir, config_dict['training_labels_dir'])
-    valid_img_dst = os.path.join(output_dir, config_dict['validation_images_dir'])
-    valid_lbl_dst = os.path.join(output_dir, config_dict['validation_labels_dir'])
+    train_img_dst = os.path.join(output_dir, config_dict["training_images_dir"])
+    train_lbl_dst = os.path.join(output_dir, config_dict["training_labels_dir"])
+    valid_img_dst = os.path.join(output_dir, config_dict["validation_images_dir"])
+    valid_lbl_dst = os.path.join(output_dir, config_dict["validation_labels_dir"])
     os.makedirs(train_img_dst, exist_ok=True)
     os.makedirs(train_lbl_dst, exist_ok=True)
     os.makedirs(valid_img_dst, exist_ok=True)
@@ -213,13 +286,17 @@ def prepare_dataset(data_directory, img_directory, lbl_directory, output_dir, tr
         shutil.move(os.path.join(lbl_src, val_labels[i]), valid_lbl_dst)
 
 
-def create_dataset_config(class_list, output_dir, config_dict):
+def create_dataset_config(class_list, output_dir, config_dict):  # pragma: no cover
     """Creates a dataset configuration (.yaml) file in the output directory"""
-    yaml_file = open(os.path.join(output_dir, config_dict['dataset_yaml_file']), 'w+')
+    yaml_file = open(os.path.join(output_dir, config_dict["dataset_yaml_file"]), "w+")
 
     # Define keys for paths to training and validation sets
-    yaml_file.write(f"{config_dict['train_key']}: {os.path.join(output_dir, config_dict['training_images_dir'])}/\n")
-    yaml_file.write(f"{config_dict['valid_key']}: {os.path.join(output_dir, config_dict['validation_images_dir'])}/\n")
+    yaml_file.write(
+        f"{config_dict['train_key']}: {os.path.join(output_dir, config_dict['training_images_dir'])}/\n"
+    )
+    yaml_file.write(
+        f"{config_dict['valid_key']}: {os.path.join(output_dir, config_dict['validation_images_dir'])}/\n"
+    )
 
     # Define keys for number of classes and class names
     yaml_file.write(f"{config_dict['num_classes']}: {len(class_list)}\n")
@@ -227,15 +304,20 @@ def create_dataset_config(class_list, output_dir, config_dict):
     yaml_file.close()
 
 
-def data_preparation_main():
+def data_preparation_main():  # pragma: no cover
     """Command line execution."""
     # Get input parameters and perform validation
     args = parse_parameters()
-    validate_arguments(args.data_dir, args.image_dir, args.label_dir, args.class_file, args.valid_size)
+    validate_arguments(
+        args.data_dir, args.image_dir, args.label_dir, args.class_file, args.valid_size
+    )
     image_formats = ["bmp", "jpg", "jpeg", "png", "tif", "tiff", "dng", "webp", "mpo"]
 
     # Read config file
-    with open(os.path.dirname(os.path.abspath(__file__)) + "/data_preparation_config.yaml", "r") as file:
+    with open(
+        os.path.dirname(os.path.abspath(__file__)) + "/data_preparation_config.yaml",
+        "r",
+    ) as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
 
     # Create lists for images, labels and classes
@@ -245,23 +327,37 @@ def data_preparation_main():
     else:
         img_dir_contents = os.listdir(args.image_dir)
         lbl_dir_contents = os.listdir(args.label_dir)
-    
+
     class_list = []
     if ".csv" in args.class_file:
-        class_list = list(pd.read_csv(args.class_file)['classes'])
+        class_list = list(pd.read_csv(args.class_file)["classes"])
     else:
         class_file = open(args.class_file)
         class_list = class_file.readlines()
-        class_list = [class_name.rstrip('\n') for class_name in class_list]
+        class_list = [class_name.rstrip("\n") for class_name in class_list]
         class_file.close()
 
-    images, labels = validate_dataset(data_dir_contents, img_dir_contents, lbl_dir_contents, image_formats, class_list)
+    images, labels = validate_dataset(
+        data_dir_contents, img_dir_contents, lbl_dir_contents, image_formats, class_list
+    )
 
     # Split dataset into training and validation sets
-    train_images, valid_images, train_labels, valid_labels = train_valid_split(images, labels, args.valid_size)
+    train_images, valid_images, train_labels, valid_labels = train_valid_split(
+        images, labels, args.valid_size
+    )
 
     # Move training and validation datasets to output directory
-    prepare_dataset(args.data_dir, args.image_dir, args.label_dir, args.output_dir, train_images, valid_images, train_labels, valid_labels, config)
+    prepare_dataset(
+        args.data_dir,
+        args.image_dir,
+        args.label_dir,
+        args.output_dir,
+        train_images,
+        valid_images,
+        train_labels,
+        valid_labels,
+        config,
+    )
 
     # Create a dataset configuration (.yaml) file in output directory
     create_dataset_config(class_list, args.output_dir, config)
