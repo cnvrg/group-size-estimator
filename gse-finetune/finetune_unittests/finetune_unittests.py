07181565140bf6bece2613841ded864c0d58e191
sjagtap1803 <siddhant.jagtap@intel.com>
2022-11-21 16:45:34 -0600

    uploaded finetuning library files

--- gse-finetune/finetune_unittests/finetune_unittests.py
+++ gse-finetune/finetune_unittests/finetune_unittests.py
@@ -15,7 +15,7 @@ class TestFineTune(unittest.TestCase):
         # Read config file for unittesting
         with open("./test_config.yaml", "r") as file:
             self.config = yaml.load(file, Loader=yaml.FullLoader)
-        
+
         # Define input arguments for unittesting
         self.false_img_path = "./images"
         self.false_lbl_path = "./labels"
@@ -25,11 +25,23 @@ class TestValidateFileLocations(TestFineTune):
     def test_dir_not_found_error(self):
         """Checks for DirectoryNotFoundError if the image or label directory does not exist"""
         with self.assertRaises(DirectoryNotFoundError):
-            validate_file_locations(self.false_img_path, self.config["labels_loc"], self.config["config_loc"])
+            validate_file_locations(
+                self.false_img_path,
+                self.config["labels_loc"],
+                self.config["config_loc"],
+            )
         with self.assertRaises(DirectoryNotFoundError):
-            validate_file_locations(self.config["images_loc"], self.false_lbl_path, self.config["config_loc"])
-    
+            validate_file_locations(
+                self.config["images_loc"],
+                self.false_lbl_path,
+                self.config["config_loc"],
+            )
+
     def test_config_not_found_error(self):
         """Checks for ConfigNotFoundError if the dataset config file does not exist"""
         with self.assertRaises(ConfigNotFoundError):
-            validate_file_locations(self.config["images_loc"], self.config["labels_loc"], self.config["config_loc"])
+            validate_file_locations(
+                self.config["images_loc"],
+                self.config["labels_loc"],
+                self.config["config_loc"],
+            )
